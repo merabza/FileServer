f5bbf42cc1c0678473ed793d4819c5851f803473
merabza <merabza@gmail.com>
2024-11-24 23:14:38 +0400

    Initial

---
+++ FileServer/Program.cs
@@ -0,0 +1,64 @@
+//Created by ApiProgramClassCreator at 11/24/2024 11:14:23 PM
+
+using ConfigurationEncrypt;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using SwaggerTools;
+using System;
+using System.Collections.Generic;
+using WebInstallers;
+using Microsoft.Extensions.Hosting;
+
+try
+{
+    var parameters = new Dictionary<string, string>
+    {
+        { ConfigurationEncryptInstaller.AppKeyKey, "29ab6e4bcd1a40d8a37ad141d59a575e" },
+        { SwaggerInstaller.AppNameKey, "File Server" },
+        { SwaggerInstaller.VersionCountKey, 1.ToString() },
+        { SwaggerInstaller.UseSwaggerWithJwtBearerKey, string.Empty } //Allow Swagger
+    };
+
+    var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        { ContentRootPath = AppContext.BaseDirectory, Args = args });
+
+    var debugMode = builder.Environment.IsDevelopment();
+
+    if (!builder.InstallServices(debugMode, args, parameters,
+
+//WebSystemTools
+            ApiExceptionHandler.AssemblyReference.Assembly,
+            ConfigurationEncrypt.AssemblyReference.Assembly,
+            SerilogLogger.AssemblyReference.Assembly,
+            StaticFilesTools.AssemblyReference.Assembly,
+            SwaggerTools.AssemblyReference.Assembly,
+            TestToolsApi.AssemblyReference.Assembly,
+            WindowsServiceTools.AssemblyReference.Assembly
+        ))
+    {
+        return 2;
+    }
+
+
+    //ReSharper disable once using
+
+    using var app = builder.Build();
+
+    if (!app.UseServices(debugMode))
+    {
+        return 1;
+    }
+
+    app.Run();
+    return 0;
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
